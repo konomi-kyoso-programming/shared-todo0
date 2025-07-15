9d456959deac93f5a68ea8da58ebc95a83201c73
konomi-kyoso-programming <konomi2010@gmail.com>
2025-07-16 08:02:28 +0900

    z

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from streamlit_gsheets import GSheetsConnection
 
 st.title("TO DO")
 #conn = st.experimental_connection("gsheets", type="GSheetsConnection")
-conn = st.connection("gsheets", type="GSheetsConnection")
+conn = st.connection("gsheets", type=GSheetsConnection)
 df = conn.read(worksheet="to-do", ttl=5)
 
 st.dataframe(df)
