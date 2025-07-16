d870a1ef8384e65febfa3c058f1407b64cf7c545
konomi-kyoso-programming <konomi2010@gmail.com>
2025-07-16 10:20:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,19 @@ from streamlit_gsheets import GSheetsConnection
 st.title("TO DO")
 #conn = st.experimental_connection("gsheets", type="GSheetsConnection")
 conn = st.connection("gsheets", type=GSheetsConnection)
-df = conn.read(worksheet="todo", ttl=5)
+df = conn.read(worksheet="sheet1", ttl=5)
 
 st.dataframe(df)
 
 
 
-STATUS = ["DONE", "TO DO", "IN PROGRESS"]
+STATUS = ["IN USE", "AVAILABLE"]
 
-with st.form(key="todo_form"):
+with st.form(key="the_form"):
     item = st.text_input(label="Item*")
     status = st.selectbox("Status", options=STATUS)
-
+    entry_date = st.date_input(label="Entry date")
+    
     submit_button = st.form_submit_button(label="Submit")
 
 if submit_button:
@@ -26,8 +27,8 @@ if submit_button:
         st.warning("you must fill the item field.")
         st.stop()
     else:
-        todo_data = pd.DataFrame([
-            {"item": item, "status": status}
+        the_data = pd.DataFrame([
+            {"item": item, "status": status, "entry_date": entry_date}
         ])
-    updated_df = pd.concat([df, todo_data], ignore_index=True)
-    conn.update(worksheet="todo", data=updated_df)
+    updated_df = pd.concat([df, the_data], ignore_index=True)
+    conn.update(worksheet="sheet1", data=updated_df)
